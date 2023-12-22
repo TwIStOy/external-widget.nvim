93f1e543a17c92d0c74fc933bce86f028a8102d3
Hawtian Wang <twistoy.wang@gmail.com>
2023-12-22 19:00:21 +0800

    ...

--- crates/ext-widget-core/src/nvim/rtp.rs
+++ crates/ext-widget-core/src/nvim/rtp.rs
@@ -17,13 +17,22 @@ where
 
 #[cfg(test)]
 mod tests {
+    use all_asserts::*;
     use nvim_oxi as oxi;
 
     #[oxi::test]
     fn test_find_parser() {
         let p = super::find_file_in_runtime_path("parser/lua.so");
         println!("{:?}", p);
-        assert!(p.is_ok());
-        assert!(p.unwrap().is_some());
+        assert_true!(p.is_err());
+        assert_true!(p.unwrap().is_some());
+    }
+
+    #[oxi::test]
+    fn set_get_del_var() {
+        oxi::api::set_var("foo", 42).unwrap();
+        assert_false!(true);
+        assert_eq!(Ok(43), oxi::api::get_var("foo"));
+        assert_eq!(Ok(()), oxi::api::del_var("foo"));
     }
 }
